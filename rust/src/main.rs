2b172f4628ab90701de6680e5d090657963bd1dd
Viktor Lazarev <taurus101v@gmail.com>
2025-06-16 00:36:04 +0200

    Generate sawtooth with PWM

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,30 +1,39 @@
 #![no_main]
 #![no_std]
 
-use cortex_m::Peripherals;
 use cortex_m_rt::entry;
 use panic_halt as _;
+use stm32f3xx_hal as hal;
 
-use stm32f3xx_hal::{delay::Delay, pac, prelude::*};
+use hal::{
+    prelude::*,
+    pac,
+};
+use stm32f3xx_hal::pwm;
 
 #[entry]
 fn main() -> ! {
-    let cp = Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
 
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.sysclk(16.MHz()).freeze(&mut flash.acr);
 
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-    let mut led = gpioa
-        .pa5
-        .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let pa7 = gpioa.pa7.into_af_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
+    let tim3_channels = pwm::tim3(dp.TIM3, 1280, 5000.Hz(),  &clocks);
 
-    let mut delay = Delay::new(cp.SYST, clocks);
+    let mut tim3_ch2 = tim3_channels.1.output_to_pa7(pa7);
+    tim3_ch2.enable();
+
+    let max_duty = tim3_ch2.get_max_duty();
+    let mut duty = 0;
 
     loop {
-        led.toggle().unwrap();
-        delay.delay_ms(500_u16);
+        tim3_ch2.set_duty(duty);
+        duty += 1;
+        if duty >= max_duty {
+            duty = 0;
+        }
     }
 }
