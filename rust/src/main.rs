ccfa54541b128c24f5c206abd9c73d6fefa50d19
Viktor Lazarev <taurus101v@gmail.com>
2025-06-16 20:54:19 +0200

    Utilize TIM8 for PWM

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -17,20 +17,20 @@ fn main() -> ! {
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
 
-    let clocks = rcc.cfgr.sysclk(16.MHz()).freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.sysclk(67.MHz()).freeze(&mut flash.acr);
 
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-    let pa7 = gpioa.pa7.into_af_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
-    let tim3_channels = pwm::tim3(dp.TIM3, 1280, 5000.Hz(),  &clocks);
+    let pa8 = gpioa.pa8.into_af_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+    let tim1_channels = pwm::tim1(dp.TIM1, 256 - 1, 300_000.Hz(), &clocks);
 
-    let mut tim3_ch2 = tim3_channels.1.output_to_pa7(pa7);
-    tim3_ch2.enable();
+    let mut tim1_ch1 = tim1_channels.0.output_to_pa8(pa8);
+    tim1_ch1.enable();
 
-    let max_duty = tim3_ch2.get_max_duty();
+    let max_duty = tim1_ch1.get_max_duty();
     let mut duty = 0;
 
     loop {
-        tim3_ch2.set_duty(duty);
+        tim1_ch1.set_duty(duty);
         duty += 1;
         if duty >= max_duty {
             duty = 0;
