94675c303d6dd5a63de969aab2d46fc071a372b3
Viktor Lazarev <taurus101v@gmail.com>
2025-07-20 13:22:39 +0200

    Implementiing processing of incomming MIDI messages

--- crates/dsp/src/main.rs
+++ crates/dsp/src/main.rs
@@ -4,14 +4,26 @@
 
 use dsp as _; // global logger + panicking-behavior + memory layout
 
-#[rtic::app(device = stm32h7xx_hal::pac, dispatchers = [], peripherals = true)]
+#[rtic::app(device = stm32h7xx_hal::pac, dispatchers = [EXTI1], peripherals = true)]
 mod app {
+    use defmt::warn;
     use dsp::state::State;
+    use midi_parser::parser::{MidiChannel, RunningStatus};
+    use rtic_sync::{
+        channel::{self, NoReceiver},
+        make_channel,
+    };
     use stm32h7xx_hal::{
-        pac,
+        nb, pac,
         prelude::*,
+        serial,
         timer::{Event, Timer},
     };
+
+    const MIDI_IN_MSG_CAPACITY: usize = 8;
+
+    type MidiRxSender = channel::Sender<'static, u8, MIDI_IN_MSG_CAPACITY>;
+    type MidiRxReceiver = channel::Receiver<'static, u8, MIDI_IN_MSG_CAPACITY>;
 
     #[shared]
     struct Shared {}
@@ -20,6 +32,9 @@ mod app {
     struct Local {
         state: State,
         sample_timer: pac::TIM2,
+        midi_rx: serial::Rx<pac::USART3>,
+        midi_parser: RunningStatus,
+        midi_rx_send: MidiRxSender,
     }
 
     #[init]
@@ -34,6 +49,7 @@ mod app {
         let rcc = dp.RCC.constrain();
         let ccdr = rcc.sys_ck(400u32.MHz()).freeze(pwrcfg, &dp.SYSCFG);
 
+        // Sample timer
         let mut timer = Timer::tim2(dp.TIM2, ccdr.peripheral.TIM2, &ccdr.clocks);
 
         timer.start(48u32.kHz());
@@ -41,11 +57,37 @@ mod app {
 
         let (sample_timer, _) = timer.free();
 
+        // MIDI
+        let gpiod = dp.GPIOD.split(ccdr.peripheral.GPIOD);
+        let _tx = gpiod.pd8.into_alternate::<7>();
+        let _rx = gpiod.pd9.into_alternate::<7>();
+
+        let serial = serial::Serial::usart3(
+            dp.USART3,
+            serial::config::Config::default().baudrate(31_250.bps()),
+            ccdr.peripheral.USART3,
+            &ccdr.clocks,
+            false,
+        )
+        .unwrap();
+
+        let (_, mut rx) = serial.split();
+        rx.listen();
+
+        // MIDI IN channel
+        let (midi_rx_send, midi_rx_recv) = make_channel!(u8, MIDI_IN_MSG_CAPACITY);
+
+        // Spawn tasks
+        process_midi_bytes::spawn(midi_rx_recv).unwrap();
+
         (
             Shared {},
             Local {
                 state: State::new(),
                 sample_timer,
+                midi_rx: rx,
+                midi_parser: RunningStatus::new(MidiChannel::Ch1),
+                midi_rx_send,
             },
         )
     }
@@ -57,5 +99,50 @@ mod app {
         loop {
             continue;
         }
+    }
+
+    #[task(binds = TIM2, priority = 9, local = [sample_timer, state])]
+    fn generator(cx: generator::Context) {
+        let tim = cx.local.sample_timer;
+        tim.sr.modify(|_, w| w.uif().clear_bit());
+
+        cx.local.state.next_sample();
+    }
+
+    #[task(binds = USART3, priority = 8, local = [midi_rx, midi_rx_send])]
+    fn midi_rx(cx: midi_rx::Context) {
+        match cx.local.midi_rx.read() {
+            Ok(byte) => {
+                match cx.local.midi_rx_send.try_send(byte) {
+                    Ok(()) => {}
+                    Err(channel::TrySendError::NoReceiver(_)) => {
+                        // todo handle the case
+                    }
+                    Err(channel::TrySendError::Full(_)) => {
+                        // todo handle the case
+                    }
+                };
+            }
+            Err(err) => match err {
+                nb::Error::Other(err) => match err {
+                    serial::Error::Framing => warn!("Error: Framing"),
+                    serial::Error::Noise => warn!("Error: Noise"),
+                    serial::Error::Overrun => warn!("Error: Overrun"),
+                    serial::Error::Parity => warn!("Error: Parity"),
+                    _ => warn!("Error: unknown"),
+                },
+                nb::Error::WouldBlock => warn!("Would block"),
+            },
+        }
+    }
+
+    #[task(priority = 7, local = [midi_parser])]
+    async fn process_midi_bytes(cx: process_midi_bytes::Context, mut recv: MidiRxReceiver) {
+        while let Ok(byte) = recv.recv().await {
+            cx.local.midi_parser.process_midi_byte(byte);
+            // todo what next?
+        }
+
+        // todo what if Err?
     }
 }
--- crates/dsp/src/state.rs
+++ crates/dsp/src/state.rs
@@ -1,6 +1,3 @@
-use defmt::info;
-use midi_parser::parser::{MidiChannel, MidiMessageKind, RunningStatus};
-
 use crate::{
     adsr::{self, TimeMs},
     consts::SAMPLE_RATE,
@@ -11,7 +8,6 @@ use crate::{
 
 pub struct State {
     pub filter: Filter,
-    pub parser: RunningStatus,
     voice_pool: VoicePool,
 }
 
@@ -28,7 +24,6 @@ impl State {
 
         Self {
             filter: Filter::new(),
-            parser: RunningStatus::new(MidiChannel::Ch1),
             voice_pool: VoicePool::new(envelope),
         }
     }
@@ -48,28 +43,28 @@ impl State {
         self.voice_pool.is_active()
     }
 
-    pub fn process_midi_byte(&mut self, byte: u8) {
-        self.parser.process_midi_byte(byte);
-        if self.parser.message_kind().is_none() {
-            return;
-        }
+    // pub fn process_midi_byte(&mut self, byte: u8) {
+    //     self.parser.process_midi_byte(byte);
+    //     if self.parser.message_kind().is_none() {
+    //         return;
+    //     }
 
-        if self.parser.in_progress() {
-            return;
-        }
+    //     if self.parser.in_progress() {
+    //         return;
+    //     }
 
-        use MidiMessageKind::*;
+    //     use MidiMessageKind::*;
 
-        match self.parser.message_kind().as_ref().unwrap() {
-            NoteOn(note, velocity) if velocity.0 > 0 => {
-                info!("Start note: {} with velocity: {}", note.num, velocity.0);
-                self.voice_pool.on_note_on(note.clone());
-            }
-            NoteOff(note, velocity) | NoteOn(note, velocity) => {
-                info!("Stop note: {} with velocity: {}", note.num, velocity.0);
-                self.voice_pool.on_note_off(note);
-            }
-            _ => {}
-        }
-    }
+    //     match self.parser.message_kind().as_ref().unwrap() {
+    //         NoteOn(note, velocity) if velocity.0 > 0 => {
+    //             info!("Start note: {} with velocity: {}", note.num, velocity.0);
+    //             self.voice_pool.on_note_on(note.clone());
+    //         }
+    //         NoteOff(note, velocity) | NoteOn(note, velocity) => {
+    //             info!("Stop note: {} with velocity: {}", note.num, velocity.0);
+    //             self.voice_pool.on_note_off(note);
+    //         }
+    //         _ => {}
+    //     }
+    // }
 }
